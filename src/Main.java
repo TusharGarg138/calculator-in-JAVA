2de54d0a4f1184f331cac1432826d290186bcfaa
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-04-16 11:48:12 +0530

    creating main method

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/*
+Name  - Tushar garg
+PRN   - 23070126138
+Batch - AIML B3
+*/
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
