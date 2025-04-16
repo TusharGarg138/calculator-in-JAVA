73f1947fc6d68f72ded5a3dcf53761697d49ac25
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-04-16 11:42:22 +0530

    taking the input with try and catch block

--- src/Input.java
+++ src/Input.java
@@ -6,5 +6,28 @@ import java.util.InputMismatchException;
 public class Input
 {
     // method to take input from user
+    int[] input() throws InvalidInputException
+    {
+        Scanner scan = new Scanner(System.in);
 
+        try
+        {
+            // input numbers
+            System.out.print("Enter num1: ");
+            int num1 = scan.nextInt();
+            System.out.print("Enter num2: ");
+            int num2 = scan.nextInt();
+
+            // create and return an array to store the numbers
+            int[] nums = new int[2];
+            nums[0] = num1;
+            nums[1] = num2;
+
+            return nums;
+        }
+        catch (InputMismatchException e)
+        {
+            throw new InvalidInputException("Invalid input! Enter valid number");
+        }
+    }
 }
